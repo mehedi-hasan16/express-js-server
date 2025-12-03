26838fb459de78901f20dd2c608d5328846ade77
mehedi-hasna16 <hasanmehedi6541@gmail.com>
2025-12-03 20:53:57 +0800

    rearrage code file

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -28,6 +28,81 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.getUser();
+    res.status(200).json({
+      success: true,
+      message: "users retrieved successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+  //   console.log(req.params.id);
+  const id = req.params.id;
+  try {
+    const result = await userServices.getSingleUser(id as string);
+    res.status(200).json({
+      success: true,
+      message: "data get by id successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  const id = req.params.id;
+  try {
+    const result = await userServices.updateUser(name, email, id!);
+    res.status(200).json({
+      success: true,
+      message: "update by id successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  //   console.log(req.params.id);
+  const id = req.params.id;
+  try {
+    const result = await userServices.deleteUser(id!);
+    res.status(200).json({
+      success: true,
+      message: "data delete by id successfully",
+      //   data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
 export const userControllers = {
   createUser,
+  getUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -5,5 +5,9 @@ import { userControllers } from "./user.controller";
 const router = Router();
 
 router.post("/", userControllers.createUser);
+router.get("/", userControllers.getUser);
+router.get("/:id", userControllers.getSingleUser);
+router.put("/:id", userControllers.updateUser);
+router.delete("/:id", userControllers.updateUser);
 
 export const userRoutes = router;
